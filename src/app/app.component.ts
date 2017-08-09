1d5c844c817766e83df66efd79b40f07675cbc22
Jakub Nowakowski <jakub.nowakowski@rb.com>
2017-08-09 17:27:54 +0200

    config service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { FrameworkConfigService } from '../fw/services/framework-config.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apka';
+  //title = 'apka';
+  constructor(private frameworkConfigService: FrameworkConfigService){
+  let config: FrameworkConfigService = {
+      socialIcons: [
+        { imageFile: 'assets/social-fb-bw.png', alt: 'Facebook', link: 'http://www.facebook.com'},
+        { imageFile: 'assets/social-google-bw.png', alt: 'Google +', link: 'http://www.google.com' },
+        { imageFile: 'assets/social-twitter-bw.png', alt: 'Twitter', link: 'http://www.twitter.com' }
+      ],
+      showLanguageSelector: true,
+      showUserControls: true,
+      showStatusBar: true,
+      showStatusBarBreakpoint: 800
+    };
+    frameworkConfigService.configure(config);
+  }
 }
