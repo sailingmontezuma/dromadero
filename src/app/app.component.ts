d2b2c70771bab173193a922de496a32b7305cc23
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-08-09 22:26:34 +0200

    config update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { FrameworkConfigService } from '../fw/services/framework-config.service';
+import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/framework-config.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,10 @@ import { FrameworkConfigService } from '../fw/services/framework-config.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- constructor(private frameworkConfigService: FrameworkConfigService) {
+  
+  constructor (private frameworkConfigService: FrameworkConfigService ) {
 
-  let config:FrameworkConfigService = {
+    let config:FrameworkConfigSettings = {
       socialIcons: [
         { imageFile: 'assets/social-fb-bw.png', alt: 'Facebook', link: 'http://www.facebook.com'},
         { imageFile: 'assets/social-google-bw.png', alt: 'Google +', link: 'http://www.google.com' },
@@ -21,6 +22,9 @@ export class AppComponent {
       showStatusBar: true,
       showStatusBarBreakpoint: 800
     };
+
     frameworkConfigService.configure(config);
+ 
+
   }
 }
