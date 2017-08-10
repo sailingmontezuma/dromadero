eae80e5adb97fe4c73b13eb82596a2f64f3519bb
Jakub Nowakowski <jakub.nowakowski@rb.com>
2017-08-10 13:55:48 +0200

    screen-below-large.directive

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -8,6 +8,8 @@ import { FrameworkConfigService } from './services/framework-config.service';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
 import { ScreenService } from './services/screen.service';
+import { ScreenBelowLarge } from './directives/screen-below-large.directive';
+import { ScreenLarge } from './directives/screen-large.directive';
 
 @NgModule({
   imports: [
@@ -18,7 +20,9 @@ import { ScreenService } from './services/screen.service';
     ContentComponent,
     TitleBarComponent,
     TopBarComponent,
-    StatusBarComponent
+    StatusBarComponent,
+    ScreenLarge,
+    ScreenBelowLarge
   ],
   providers: [
       FrameworkConfigService,
