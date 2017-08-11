af538d22138645113219dd3492834d92ee72c52e
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-08-11 16:14:20 +0200

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FwModule } from '../fw/fw.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CountriesComponent } from './countries/countries.component';
 import { SettingsComponent } from './settings/settings.component';
+import { appRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { SettingsComponent } from './settings/settings.component';
     BrowserModule,
     HttpModule,
     FormsModule,
-    FwModule
+    FwModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
 
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
@@ -14,7 +15,8 @@ import { MenuService } from './services/menu.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     FrameworkBodyComponent,
