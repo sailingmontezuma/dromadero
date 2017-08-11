208c1d80a0b0131f860cad2e3e85a907716f9abe
Jakub Nowakowski <jakub.nowakowski@rb.com>
2017-08-11 12:03:21 +0200

    fff

--- src/fw/directives/screen-large.directive.ts
+++ src/fw/directives/screen-large.directive.ts
@@ -13,25 +13,31 @@ export class ScreenLarge implements OnDestroy {
     this.screenSubscription = screenService.resize$.subscribe(() => this.onResize());
   }
 
-  @Input()
-  set screenLarge(condition) {
-    // ignore the passed condition and set it based on screen size
-    condition = this.screenService.screenWidth >= this.screenService.largeBreakpoint;
+@Input()
+set screenLarge(condition) {
+  // ignore the passed condition and set it based on screen size
+  condition = this.screenService.screenWidth >= this.screenService.largeBreakpoint;
 
-    if (condition && !this.hasView) {
-      this.hasView = true;
-      this.viewContainer.createEmbeddedView(this.template);
-    } else if (!condition && this.hasView) {
-      this.hasView = false;
-      this.viewContainer.clear();
-    }
+  if (condition && !this.hasView) {
+    this.hasView = true;
+    this
+      .viewContainer
+      .createEmbeddedView(this.template);
+  } else if (!condition && this.hasView) {
+    this.hasView = false;
+    this
+      .viewContainer
+      .clear();
   }
+}
 
-  ngOnDestroy() {
-    this.screenSubscription.unsubscribe();
-  }
+ngOnDestroy() {
+  this
+    .screenSubscription
+    .unsubscribe();
+}
 
-  onResize() {
+onResize() {
     // trigger the setter
     this.screenLarge = false;
   }
