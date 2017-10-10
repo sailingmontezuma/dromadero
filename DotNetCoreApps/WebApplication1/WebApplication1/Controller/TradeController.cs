2165a8c862295f1d11d26cba6959d51663e80e08
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-10-10 23:50:01 +0200

    ef

--- DotNetCoreApps/WebApplication1/WebApplication1/Controller/TradeController.cs
+++ DotNetCoreApps/WebApplication1/WebApplication1/Controller/TradeController.cs
@@ -9,9 +9,10 @@ namespace WebApplication1.Controllers
 {
     public class TradeController : Microsoft.AspNetCore.Mvc.Controller
     {
+        private readonly ICategoryRepository _categoryRepository;
         private readonly ITradeRepository _tradeRepository;
 
-        public TradeController(ITradeRepository tradeRepository)
+        public TradeController(ITradeRepository tradeRepository, ICategoryRepository categoryRepository)
         {
             _tradeRepository = tradeRepository;
         }
--- DotNetCoreApps/WebApplication1/WebApplication1/Models/Trade.cs
+++ DotNetCoreApps/WebApplication1/WebApplication1/Models/Trade.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
     public class Trade
     {
         public int Id  { get; set; }
+        public string Symbol { get; set; }
+        public string TransactionType { get; set; }
         public DateTime Open { get; set; }
         public DateTime Close { get; set; }
         public decimal PriceOpen { get; set; }
         public decimal PriceClose { get; set; }
-
         public bool IsTradeOfTheMonth { get; set; }
         public virtual Category Category { get; set; }
     }
