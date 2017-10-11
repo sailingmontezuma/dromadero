e5cd7671ad67228547b959af567d0b2b0a852fbe
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-10-11 08:38:43 +0200

    images

--- DotNetCoreApps/WebApplication1/WebApplication1/Models/Trade.cs
+++ DotNetCoreApps/WebApplication1/WebApplication1/Models/Trade.cs
@@ -5,6 +5,8 @@ namespace WebApplication1.Models
     public class Trade
     {
         public int Id  { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public string Symbol { get; set; }
         public string TransactionType { get; set; }
         public DateTime Open { get; set; }
@@ -12,6 +14,8 @@ namespace WebApplication1.Models
         public decimal PriceOpen { get; set; }
         public decimal PriceClose { get; set; }
         public bool IsTradeOfTheMonth { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
     }
 
