870eefc45afa95545b5eb23d09e23eea5d79ba8c
Jakub Nowakowski <jakub.nowakowski@rb.com>
2017-10-11 14:38:53 +0200

    azure web app

--- DotNetCoreApps/WebApplication1/WebApplication1/Startup.cs
+++ DotNetCoreApps/WebApplication1/WebApplication1/Startup.cs
@@ -1,23 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
 using WebApplication1.Models;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
             services.AddTransient<ICategoryRepository, MockCategoryRepository>();
             services.AddTransient<ITradeRepository, MockTradeRepository>();
             services.AddMvc();
@@ -30,7 +37,7 @@ namespace WebApplication1
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
-            
         }
+
     }
 }
