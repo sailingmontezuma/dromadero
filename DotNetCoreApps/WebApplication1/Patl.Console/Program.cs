df7750251d08128ef84f0221cbc3b680fc11a532
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-10-15 21:21:43 +0200

    projects merge

---
+++ DotNetCoreApps/WebApplication1/Patl.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patl.UiConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- DotNetCoreApps/WebApplication1/Patl.Data/StrategyContext.cs
+++ DotNetCoreApps/WebApplication1/Patl.Data/StrategyContext.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Patl.Domain;
 
 namespace Patl.Data
 {
     public class StrategyContext : DbContext
     {
+        private IConfigurationRoot _config;
+
+        public StrategyContext(IConfigurationRoot config, DbContextOptions options) : base(options)
+        {
+            _config = config;
+        }
+
         public DbSet<Strategy> Strategies { get; set; }
         public DbSet<Trade> Trades { get; set; }
-
         //_context.Strategies.ToList();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=FxData2; Trusted_Connection=True;");
- 
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:DefaultConnection"]);
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=FxData2; Trusted_Connection=True;");
         }
     }
 }
